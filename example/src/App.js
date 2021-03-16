f6170b5db23d4700a4215c7935b268afcb46804b
Kais3rP <cesare.polonara@gmail.com>
2021-03-16 23:27:30 +0100

    fixed example

--- example/src/App.js
+++ example/src/App.js
@@ -24,7 +24,7 @@ text-align: center;
 const functionExample = `function Test () {
   return (
     <div className="__custom__live__react__">
-      <h5>Functional Component
+      <h5>Functional Component</h5>
     </div>
     )
 }`;
