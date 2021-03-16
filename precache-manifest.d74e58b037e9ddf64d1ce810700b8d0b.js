fbb7036d3bf0b7d00bdc57c4f05bdeee59ca1c6c
Kais3rP <cesare.polonara@gmail.com>
2021-03-16 23:18:29 +0100

    Updates

---
+++ precache-manifest.d74e58b037e9ddf64d1ce810700b8d0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02a4e4de2b7e0b61474b8b3771d0aa8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c74be38290a0d4040348",
+    "url": "./static/css/2.848dce0b.chunk.css"
+  },
+  {
+    "revision": "a437f68df5725e3bfc85",
+    "url": "./static/css/main.66596649.chunk.css"
+  },
+  {
+    "revision": "c74be38290a0d4040348",
+    "url": "./static/js/2.04441afa.chunk.js"
+  },
+  {
+    "revision": "f9a822354edbac8758c25bf2d863eabf",
+    "url": "./static/js/2.04441afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a437f68df5725e3bfc85",
+    "url": "./static/js/main.903f3edc.chunk.js"
+  },
+  {
+    "revision": "1d9f907fb901ce663f93",
+    "url": "./static/js/runtime-main.047c9674.js"
+  }
+]);
