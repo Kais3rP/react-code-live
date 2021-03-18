895665ce1276d2f6e2ea335a9741a0ddc5cee553
Kais3rP <cesare.polonara@gmail.com>
2021-03-18 16:33:36 +0100

    added error container dataset attribute

--- src/components/error-logger/ErrorLogger.js
+++ src/components/error-logger/ErrorLogger.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ErrorLogger({ error, ...props }) {
   return (
-    <div {...props}>
+    <div data-id='error-container' {...props}>
       <h4>{error}</h4>
     </div>
   )
